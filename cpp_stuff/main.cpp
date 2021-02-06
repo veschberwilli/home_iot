671b654bbb68020aa2eb61269762b0997dffd047
Dr. Ing. Michael Mink <micmink@cmtcleu57074552.rd.corpintra.net>
2021-02-06 23:04:35 +0100

    update

--- cpp_stuff/main.cpp
+++ cpp_stuff/main.cpp
@@ -3,11 +3,15 @@
 
 
 #include <iostream>
+#include <vector>
 #include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+#include "ExifTool.h"
 
 
 using namespace std;
 namespace po = boost::program_options;
+namespace fs = boost::filesystem;
 
 
 po::variables_map getProgramOptions(const int argc, const char* const* argv) {
@@ -16,7 +20,7 @@ po::variables_map getProgramOptions(const int argc, const char* const* argv) {
     program_options.add_options()
         ("help,h", "Display the help message.")
         ("name,n", po::value<string>()->default_value("na"), "Select Name to Search For.")
-        ("dir,d", po::value<string>()->default_value("./"), "Select Directory to Search In.")
+        ("dir,d", po::value<string>()->default_value("../pics"), "Select Directory to Search In.")
         ("recursive,r", po::value<bool>()->default_value("True"), "Search Recursive.");
     
     po::variables_map vm;
@@ -40,15 +44,56 @@ void printHeader(){
 int main(int argc, char *argv[]) {
 
     // print header
-    printHeader;
+    printHeader();
 
     // parse cmd line params
     po::variables_map vm = getProgramOptions(argc, argv);
 
-    // loop over files and extract exif info and store in array
+    // print params
     cout << "Name:" << vm["name"].as<string>() << endl;
     cout << "Dir:" << vm["dir"].as<string>() << endl;
-    cout << "Recursive:" << vm["recursive"].as<bool>() << endl;
+    cout << "Recursive:" << vm["recursive"].as<bool>() << "\n" << endl;
+
+    // loop over files and extract exif info and store in array
+    fs::path p = vm["dir"].as<string>();
+    vector<string> white_list = {".jpg", ".png"};
+    
+    if (fs::is_directory(p))
+    {
+        for (fs::directory_entry& entry : fs::recursive_directory_iterator(p))
+        {
+            if ((entry.path().has_extension()) && (find(white_list.begin(), white_list.end(), entry.path().filename()) == white_list.end()))
+            {
+                cout << "Path: " << entry.path() << endl;
+                cout << "Filename: " << entry.path().filename().c_str() << endl;
+                cout << "Extension: " << entry.path().extension() << endl;
+
+                //char tmp[] entry.path().filename().c_str();
+
+                // exiftool
+                ExifTool *et = new ExifTool();
+                // read metadata from the image
+                TagInfo *info = et->ImageInfo(entry.path().c_str());
+                if (info) 
+                {
+                    // print returned information
+                    for (TagInfo *i=info; i; i=i->next) 
+                    {
+                        cout << i->name << " = " << i->value << endl;
+                    }
+                    // we are responsible for deleting the information when done
+                    delete info;
+                }
+                
+            }
+        }
+    }
+        
+        
+
+    
+    
+
 
     // check array and report the oldest time stamp/file
 
