1a930efb4a5224052b35a3ba721a37b0b2b0755a
Michael Mink <michael.mink@daimler.com>
2021-11-15 21:44:31 +0100

    mv stuff

--- ros/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/egon/repos/home_iot/ros/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
--- udemy_cpp/file_streams/case_study_coin_revision/build.sh
+++
@@ -1,1 +0,0 @@
-g++ main.cc coin.cc -o main
